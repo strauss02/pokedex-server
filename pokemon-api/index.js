bb7897de542245c8df6f575441836183160ac89c
Ido Strauss <strauss02@hotmail.co.il>
2021-10-21 22:02:35 +0300

    accomplish get method

--- pokemon-api/index.js
+++ pokemon-api/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const pokemonRouter = require('./routers/pokemonRouter')
+const Pokedex = require('pokedex-promise-v2')
+const P = new Pokedex()
 
 //start server listening
 app.listen(port, () => console.log('listening on', port))
@@ -8,4 +11,18 @@ app.listen(port, () => console.log('listening on', port))
 //route the app
 app.get('/', (req, res) => {
   res.send('we got a get request ')
+})
+
+//send back pokemon name according to id or name requested
+// app.get('/:id', pokemonRouter)
+
+//send back pokemon name according to id or name requested
+app.get('/:id', (req, res) => {
+  console.log('we got id param')
+  console.log(req.params.id)
+  P.getPokemonByName(req.params.id)
+    .then((pokeres) => {
+      res.send(pokeres.name)
+    })
+    .catch((err) => res.send(err.message))
 })
